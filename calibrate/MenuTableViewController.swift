fff3e1fd1b2323fe0d7302783448130360b09d8b
Oliver Eichhorn <oeichhorn@me.com>
2015-10-26 15:38:41 +0100

    New menu controller with pictures now.

--- calibrate/MenuTableViewController.swift
+++ calibrate/MenuTableViewController.swift
@@ -14,6 +14,15 @@ class MenuTableViewController: UITableViewController {
 	@IBOutlet weak var calibrateNavigationItem: UINavigationItem!
 	var detailViewController: DetailViewController?
 	
+	@IBOutlet weak var tableCellLabelIntro: UILabel!
+	@IBOutlet weak var tableCellLabel1: UILabel!
+	@IBOutlet weak var tableCellLabel2: UILabel!
+	@IBOutlet weak var tableCellLabel3: UILabel!
+	@IBOutlet weak var tableCellLabel4: UILabel!
+	@IBOutlet weak var tableCellLabel5: UILabel!
+	@IBOutlet weak var tableCellLabel6: UILabel!
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,12 +46,33 @@ class MenuTableViewController: UITableViewController {
 		}
 
 		calibrateNavigationItem.titleView = logoImageView
+		
+		// set localized labels
+		
+		
+		tableCellLabelIntro.text = NSLocalizedString("Introduction", comment: "")
+		
+		tableCellLabel1.text = "1. " + NSLocalizedString("Brightness", comment: "")
+		tableCellLabel2.text = "2. " + NSLocalizedString("Contrast", comment: "")
+		tableCellLabel3.text = "3. " + NSLocalizedString("Verification", comment: "")
+		tableCellLabel4.text = "4. " + NSLocalizedString("Colorsteps", comment: "")
+		tableCellLabel5.text = "5. " + NSLocalizedString("Saturation", comment: "")
+		tableCellLabel6.text = "6. " + NSLocalizedString("Sharpness", comment: "")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+	
+	override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		if section == 1 {
+			return NSLocalizedString("Calibration", comment: "")
+		}
+		
+		return ""
+	}
+	
 	override func tableView(tableView: UITableView, didUpdateFocusInContext context: UITableViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
 		
 		// Check that the next focus view is a child of the table view.
@@ -51,7 +81,7 @@ class MenuTableViewController: UITableViewController {
 				let nextFocusedIndexPath = context.nextFocusedIndexPath,
 				let	detailViewController = detailViewController else { return }
 		
-		detailViewController.setCurrentItem(nextFocusedIndexPath)
+		detailViewController.setItem(nextFocusedIndexPath)
 	}
 	
 }
